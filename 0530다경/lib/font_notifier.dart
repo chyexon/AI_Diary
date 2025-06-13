6ecd1506464493103cbc4fb7473dfacd2e61c2eb
kkangdaeun <164451449+kkangdaeun@users.noreply.github.com>
2025-06-13 14:22:18 +0900

    Delete 0530다경 directory

--- 0530다경/lib/font_notifier.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-final fontNotifier = ValueNotifier<String>('CookieRun');
-
-extension FontChanger on ValueNotifier<String> {
-  void changeFont(String newFont) {
-    value = newFont;
-  }
-}
